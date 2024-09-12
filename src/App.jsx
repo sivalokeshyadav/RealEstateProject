237d7579dc7b13cb6bc250f5ec9105a81074e3ec
Sivalokesh Yadav <222sivalokeshyadav@gmail.com>
2024-09-12 15:57:48 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-//import React from 'react'
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 const App = () => {
   return (
-    <div className="text-red-600">
-      <h1>App</h1>
-    </div>
+
+<BrowserRouter>
+<Header />
+<Routes>
+  <Route  path="/" element={<Home />} />
+  <Route  path="/signup" element={<Signup />} />
+  <Route  path="/signin" element={<Signin />} />
+  <Route  path="/about" element={<About />} />
+  <Route  path="/profile" element={<Profile />} />
+</Routes>
+</BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+
+const Home = () => {
+  return (
+    <div className="text-blue-600">Home</div>
+  )
+}
+
+export default Home
